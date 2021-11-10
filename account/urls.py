68ffb94bed6f5423f3d5530dae603bdb7ed7a7d7
Devpal <devdivakar8@gmail.com>
2021-11-10 23:11:38 +0530

    copy link added

--- account/urls.py
+++ account/urls.py
@@ -6,8 +6,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('signup/', views.signup, name="signup"),
+    path('signup/<str:username>', views.signupLink, name="signuplink"),
     path('signin/', views.signin, name="signin"),
     path('logout/', auth_views.LogoutView.as_view(template_name='logout.html'),name='logout'),
-
-
 ]
--- account/views.py
+++ account/views.py
@@ -81,6 +81,72 @@ def signup(request):
 
     return render(request,'signup.html')
 
+def signupLink(request,username):
+    if request.user.is_authenticated:
+        logout(request)
+    if request.method == 'POST':
+        sponsor = request.POST.get('sponsorid')
+        mobile_no = request.POST.get('mobile')
+        email = request.POST.get('email')
+        password = request.POST.get('pass')
+        txnPassword = request.POST.get('txnpass')
+        fullName = request.POST.get('fullname')
+        
+
+        hax_password = make_password(password)
+
+        if password != txnPassword:
+            try:
+                spn_obj = Account.objects.get(username=sponsor)
+                while True:
+                    rand_num = random.randint(500000,599999)
+                    u_name = 'BPM' + str(rand_num)
+                    if Account.objects.filter(username=u_name).exists():
+                        pass
+                    else:
+                        break
+
+                user = Account(username = u_name, sponsor=spn_obj,
+                                password=hax_password,email=email,
+                                txn_password=txnPassword,phon_no=mobile_no,
+                                password1=password,first_name=fullName,
+                                last_name="lName")
+
+                user.save()
+                fund = Fund(user = user)
+                fund.save()
+                request.session['user_name'] = user.username
+                request.session['password'] = password
+
+                # try:
+                #     # send msg to that no.........
+                #     client = Client(settings.TWILIO_S_ID, settings.TWILIO_AUTH_TOKEN)
+                #     message = client.messages.create(
+                #                         body="Welcome to Bitpaymax " + "username = " + user.username + " Password = " + str(password) + " Txnpass = "+str(txnPassword),
+                #                         from_='+13526676754',
+                #                         to='+91' + mobile_no
+                #                     )
+                #     print("msg send successs")
+                # except Exception as e:
+                #     print("no error",e)
+
+                # Send Id and pass on Mobile number with id and password.....
+                messages.success(request,'User Created Successfully!!\nusername : '+user.username +'\nPassword: '+password+'\nTxnPassword: '+txnPassword)
+                return redirect('signin')
+
+
+            except Exception as e:
+                print(e)
+                messages.error(request,"Sponsor Does Not Exist!!")
+                return redirect('signup')
+        else:
+            messages.error(request,'Password and transection password can not be same!!')
+            return redirect('signup')
+
+    else:
+        pass
+    return render(request,'signup.html',{'sponsor_username': username})
+
 def signin(request):
     try:
         user_name = request.session['user_name']
